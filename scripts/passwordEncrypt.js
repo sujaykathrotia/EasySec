8b43976ca85f4277bafe8604ae4279a5db0987ce
OpenShift guest <mohd.husn001@gmail.com>
2013-08-17 14:10:25 -0400

    test enc

--- scripts/passwordEncrypt.js
+++ scripts/passwordEncrypt.js
@@ -1,9 +1,44 @@
 var password = 'pass';
-var key = '';
+password = doEncryption(password);
+console.log(password);
+password = doDecryption(password);
+console.log(password);
 
-password =formatPlaintext(password);
-key = getRandomBytes(12);
+function toHex(str) {
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        hex += ''+str.charCodeAt(i).toString(16);
+    }
+    return hex;
+}
 
-var res = rijndaelEncrypt(password,key,"CBC");
+function doEncryption(passwordPlainText)
+{
+  var pt, key;
+  blockSizeInBits=128;
+  keySizeInBits =128;
+ 
+  key = 'E8E9EAEBEDEEEFF0F2F3F4F5F7F8F9FA';
+     
+  pt = hex2s(toHex(passwordPlainText));
+  key = hex2s(key);
+  var passwordEnc = byteArrayToHex(rijndaelEncrypt(pt, key, "ECB"));
 
-console.log(res);
+  return passwordEnc;
+}
+
+function doDecryption(passwordEnc) {
+  var ct, key;
+  var theForm = document.forms[0];
+  blockSizeInBits=128;
+  keySizeInBits = 128;
+  
+  
+  key = 'E8E9EAEBEDEEEFF0F2F3F4F5F7F8F9FA';
+
+  ct = hex2s(toHex(passwordEnc));
+  key = hex2s(theForm.key.value);
+  var passwordPlainText = byteArrayToHex(rijndaelDecrypt(ct, key, "ECB"));
+
+  return passwordPlainText;
+}
