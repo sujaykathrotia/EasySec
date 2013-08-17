ec9c90c82ecf3dcc684947a458ce2036e42ae139
Mohit <mohit.shah41@gmail.com>
2013-08-17 12:30:19 -0400

    link expansion code done.

--- scripts/LinkExpansion.js
+++ scripts/LinkExpansion.js
@@ -5,12 +5,10 @@ $(function(){
 		working_image: 'http://longurl.org/static/ajax_indicator_sm_round.gif'
 	};
 	
-	var links = this;
-	var known_services = [];
+	known_services = [];
 	var data_cache = [];
 
 	getServicesFromAPI = function() {
-		console.log("Sdfdf");
 		jQuery.ajax({
 			type: 'GET',
 			url: 'http://api.longurl.org/v1/services?format=json',
@@ -34,35 +32,64 @@ $(function(){
 						known_services.push(data[x][y]);
 					}
 				}
+				
 			}
 		}
 		
 		known_services = known_services.sort();
-		console.log(known_services);
-	}
 
-getServicesFromAPI();
+		checkLinks();
+	};
 
-	shorturlDomains = [ "bit.ly", "tinyurl.com","on.fb.me"];
+	//get all tiny urls
+	getServicesFromAPI();
+	//get all urls from page
 	allLinks = $('a');
-	allLinks.each(function(){
+
+	checkLinks = function() {
+		allLinks.each(function(){
 		if($(this).attr('href')){
 			var hrefl=$(this).attr('href');
 			if(checkShorturlDomains(hrefl)){
-				//$(this).longurl();
-				$(this).attr('href',"google.com");
-				$(this).text("google.com");
-			//	ExpandLink(url);
+			url =Expandlink(hrefl);
+			
+			$(this).attr('href',url);
+			$(this).text(url);
 			}
 		}}); 
+	}
+
+	
 	function checkShorturlDomains(href){
-		for(var j=0; j<shorturlDomains.length; j++){
+		for(var j=0; j<known_services.length; j++){
 		//if link matches with shorturldomain
 		if(href.indexOf(known_services[j])!=-1){
 			return true;
 		}
 	}
 	return false;
+}
+
+function Expandlink(url){
+	var longurl;
+	var apiURL = "http://api.longurl.org/v2/expand?url="+ url + "&format=json";
+	
+	$.ajax({
+	url: apiURL,
+	dataType: "json",
+	async:false,
+	success: function(data){
+		
+		if(data["type"]!="error"){
+		longurl = data["long-url"];
+		}
+		else{
+			longurl=url;
+		}
+	}
+	});
+	return longurl;
+	
 }
 
 });
