78eab3034dfa481808848b77b93ef01f2bbc4895
Shabbir Hussain <mohd.husn001@gmail.com>
2013-08-17 12:30:26 -0400

    added enc files

--- scripts/passwordEncrypt.js
+++ scripts/passwordEncrypt.js
@@ -7,8 +7,8 @@ for(var i = 0; i < 32; i++)
   key[i] = i;
 
 AES_ExpandKey(key);
-AES_Encrypt(block, key);
+var mystring = AES_Encrypt(block, key);
 
-console.log(block);
+console.log(mystring);
 
 AES_Done();
