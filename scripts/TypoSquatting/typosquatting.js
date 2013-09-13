bdf2b31bae4e9236256e537f21ff9691dd21213a
Sujay Kathrotia <sujaykathrotia@gmail.com>
2013-09-13 05:26:50 -0400

    TypoSquatting object created

--- scripts/TypoSquatting/typosquatting.js
+++ scripts/TypoSquatting/typosquatting.js
@@ -1,89 +1,48 @@
-$(function(){
-	
-	//check if first time using
-	chrome.storage.local.get('first',function(data){
-	
-		if(data.first=='pup'){
-			//not the first time; use saved dictionary
-			chrome.storage.local.get('dictionary',function(data){
-				var dictionary = data.dictionary;
-				getURLandCheck(dictionary);
-			});
-		}else{
-		//store a dictionary for first use
-			var dictionary=["facebook.com","google.ca","youtube.com","microsoft.com","apple.com","yahoo.com","baidu.com","wikipedia.org","qq.com","linkedin.com","amazon.com","amazon.ca","live.com","twitter.com","blogspot.com","taobao.com","kijiji.com","bing.com","yandex.com","ebay.com","tumblr.com","mcgill.ca","wordpress.com","gc.ca","pinterest.com","craigslist.ca","craigslist.com","tdcanadatrust.com","paypal.com","msn.com","imgur.com","imdb.com","ebay.com","ebay.ca","td.com","instagram.com","msn.ca","reddit.com","cnn.com","cbc.ca","googleusercontent.com","royalbank.com","thepiratebay.sx","theweathernetwork.com","conduit.com","netflix.com","about.com","godaddy.com","bbc.co.uk","huffingtonpost.com","xvideos.com","xhamster.com","flickr.com","bmo.com","craigslist.org","pornhub.com","royalbank.com","stackoverflow.com","theglobeandmail.com","ask.com","vube.com","wordpress.org","thestar.com","go.com","scotiabank.com","pof.com","realtor.ca","cibc.com","blogger.com","hootsuite.com","aweber.com","adobe.com","fiverr.com","cyberpresse.ca","secureserver.net","futureshop.ca","dailymail.co.uk","reference.com","vimeo.com","akamaihd.net","rogers.com","kickass.to","indeed.com","redflagdeals.com","cnet.com","yellowpages.ca","nytimes.com","bestbuy.com","bell.ca","canadapost.ca","espn.go.com","amazonaws.com","livejasmin.com","canadiantire.ca","yelp.ca","youporn.com","pcfinancial.ca","nationalpost.com","tripadvisor.ca","warriorforum.com"];
-			
-			chrome.storage.local.set({'dictionary':dictionary},function(){});
-			
-			getURLandCheck(dictionary);
-		}
-	});
-	
-	chrome.storage.local.set({'first':'pup'},function(){});
-	
-function getURLandCheck(urlsDictionary){	
-				
-		//get page url
-		url=window.location.href;
-		
-		//get domain only
-		if(url.indexOf("http:")!=-1){
+var _TySq = {};
 
-			url=url.substring(7);
-		}
-		else if(url.indexOf("https:")!=-1){
-			url=url.substring(8);
-		}
+_TySq.topDomains = ["facebook.com","google.com","youtube.com","microsoft.com","apple.com","yahoo.com","baidu.com","wikipedia.org","qq.com","linkedin.com","amazon.com","amazon.ca","live.com","twitter.com","blogspot.com","taobao.com","kijiji.com","bing.com","yandex.com","ebay.com","tumblr.com","mcgill.ca","wordpress.com","gc.ca","pinterest.com","craigslist.ca","craigslist.com","tdcanadatrust.com","paypal.com","msn.com","imgur.com","imdb.com","ebay.com","ebay.ca","td.com","instagram.com","msn.ca","reddit.com","cnn.com","cbc.ca","googleusercontent.com","royalbank.com","thepiratebay.sx","theweathernetwork.com","conduit.com","netflix.com","about.com","godaddy.com","bbc.co.uk","huffingtonpost.com","xvideos.com","xhamster.com","flickr.com","bmo.com","craigslist.org","pornhub.com","royalbank.com","stackoverflow.com","theglobeandmail.com","ask.com","vube.com","wordpress.org","thestar.com","go.com","scotiabank.com","pof.com","realtor.ca","cibc.com","blogger.com","hootsuite.com","aweber.com","adobe.com","fiverr.com","cyberpresse.ca","secureserver.net","futureshop.ca","dailymail.co.uk","reference.com","vimeo.com","akamaihd.net","rogers.com","kickass.to","indeed.com","redflagdeals.com","cnet.com","yellowpages.ca","nytimes.com","bestbuy.com","bell.ca","canadapost.ca","espn.go.com","amazonaws.com","livejasmin.com","canadiantire.ca","yelp.ca","youporn.com","pcfinancial.ca","nationalpost.com","tripadvisor.ca","warriorforum.com"];
 
-		if(url.indexOf("www")!=-1){
-			url=url.substring(4);
-		}
+var current_domain = location.href.match(/^(?:https?:\/\/)?(?:www\.)?((?:[-\w]+\.)+[a-zA-Z]{2,})(\/.+)?/i)[1];
 
-		if(url.indexOf("/")!=-1){
-			url=url.substring(0,url.indexOf("/"));
-		}
-		else if(url.indexOf("?")!=-1){
-			url=url.substring(0,url.indexOf("?"));
-		}
-		//console.log(url);
+_TySq.onLoad = function(data) {
+	_TySq.getURLandCheck();
+};
 
-	//console.log(checkDistance(url));
-	var matchBest = checkDistance(url,urlsDictionary);
+_TySq.getURLandCheck = function () {
+	var matchBest = _TySq.checkDistance(current_domain, _TySq.topDomains);
 	if(matchBest !== "") {
-		var style = 'position: fixed; width: 100%; background-color:#d14836; color: #fafafa;font-size: 24px; padding: 20px;z-index:99999; border-bottom: 5px solid #fff;';
-		var a = $("<div style='" + style + "'>Are you sure you want to browse <b>" + url + " </b> instead of <a target='_blank' style='color: #fafafa;' href='http://" + matchBest + "'><b>" + matchBest + "</b></a> ?</div>");
+		var style = 'position: fixed; top: 0px; width: 100%;background: linear-gradient(to bottom, #D14836 0%,#CC0D00 100%);color: #fafafa;font-size: 13px;padding: 6px 15px;z-index: 99999;box-shadow: 0px 0px 15px #333;border-bottom: 2px solid #D60000;';
+		var a = $("<div style='" + style + "'>Are you sure you want to browse <b>" + current_domain + " </b> instead of <a target='_blank' style='color: #fafafa;' href='http://" + matchBest + "'><b>" + matchBest + "</b></a> ?</div>");
+		
 		$("body").prepend(a);
 		
-		
 		var ignore = $("<span style='float: right; font-weight: bold;margin-right: 50px;cursor: pointer;'>ignore<span>");
-		ignore.on("click", function() { a.hide();});
-		a.append(ignore);
-		
-		var addtodic = $("<span style='float: right; font-weight: bold;margin-right: 50px;cursor: pointer;'>add To dictionary<span>");
-		addtodic.on("click", function() { 
-			
-			urlsDictionary.push(url);
-			chrome.storage.local.set({'dictionary':urlsDictionary},function(){});
+		ignore.on("click", function() {
 			a.hide();
+			chrome.extension.sendRequest({
+				action: 'setOptions',
+				domain: current_domain,
+				module: "_TySq",
+				option: "1"
+			}, function (){});
 		});
-		a.append(addtodic);
 		
+		a.append(ignore);
 	}
-}
-function checkDistance(url,urlsDictionary){
-	var likelyMatch="";
-	for (var i = 0; i <urlsDictionary.length;i++) {
-		t=getEditDistance(url,urlsDictionary[i]);
-		
+};
+
+_TySq.checkDistance = function (url, urlsDictionary) {
+	var likelyMatch = "";
+	for (var i = 0; i < urlsDictionary.length; i++) {
+		t = getEditDistance(url,urlsDictionary[i]);
 		//if full match then return safe
-		if(t==0){
+		if(t === 0) {
 			return "";
 		}
 		//if typo, save then keep searching through dictionary
-		if(t==1 || t==2 ||t==3){
+		if(t == 1 || t == 2 || t == 3) {
 			likelyMatch = urlsDictionary[i];
-		}	
-	};
+		}
+	}
 	return likelyMatch;
-}
-});
+};
