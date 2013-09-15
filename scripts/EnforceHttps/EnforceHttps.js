9c1028a000bdd829a72a552c6a55bac81d75d5a3
Sujay Kathrotia <sujaykathrotia@gmail.com>
2013-09-15 10:18:34 +0530

    Enforce HTTPS - Save Domain Support value in settings object

--- scripts/EnforceHttps/EnforceHttps.js
+++ scripts/EnforceHttps/EnforceHttps.js
@@ -1,23 +1,18 @@
 var _EnHt = {};
 
-_EnHt.onLoad = function(){
-
+_EnHt.onLoad = function() {
 	//enforce if not https
-	if (window.location.protocol != "https:"){
-	    
+	if (window.location.protocol != "https:") {
 		var httpsURL = "https:" + window.location.href.substring(window.location.protocol.length);
-
 		var me = this;
 		/* Asking Background to check if site supports https*/
 		chrome.extension.sendRequest({
 			action: 'checkHTTPS',
 			link:  httpsURL
 		}, _EnHt.redirect);
-			
 	}
 };
 
-_EnHt.redirect = function(){
-
+_EnHt.redirect = function() {
 	window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
 };
