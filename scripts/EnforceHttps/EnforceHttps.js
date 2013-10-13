63a8bb6fca7f7569cf5201856f19c9277a2c107c
Shabbir Hussain <mohd.husn001@gmail.com>
2013-10-13 11:05:11 -0400

    fixed httpsEnforce

--- scripts/EnforceHttps/EnforceHttps.js
+++ scripts/EnforceHttps/EnforceHttps.js
@@ -1,19 +1,34 @@
 var _EnHt = {};
 
 _EnHt.onLoad = function(){
-
+	console.log('alive');
+	
 	//enforce if not https
 	if (window.location.protocol != "https:"){
 	    
 		var httpsURL = "https:" + window.location.href.substring(window.location.protocol.length);
-
-		var me = this;
-		/* Asking Background to check if site supports https*/
-		chrome.extension.sendRequest({
-			action: 'checkHTTPS',
-			link:  httpsURL
-		}, _EnHt.redirect);
-			
+		var httpURL = window.location.href;
+		
+		value = localStorage['httpsAttempts'];
+		console.log(value);
+		if(value >= 1){
+			//do nohing
+			console.log('donothing');
+		}
+		else{
+			var me = this;
+			localStorage['httpsAttempts']=1;
+			/* Asking Background to check if site supports https*/
+			chrome.extension.sendRequest({
+				action: 'checkHTTPS',
+				link:  httpsURL
+			}, _EnHt.redirect);
+		}
+					
+	}
+	else{
+		
+		localStorage['httpsAttempts']=0;
 	}
 };
 
